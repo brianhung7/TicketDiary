3d32f9e91cdf7c6973b23c65fd5dee6e169ecac1
brianhung7 <brian.texts@gmail.com>
2021-07-30 12:25:27 -0700

    installed dependencies

--- config/db.connection.js
+++ config/db.connection.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 /* NOTE this is our db connection string */
-require('dotenv').config();
-const connectionStr = process.env.MONGODB_URI || 'mongodb://localhost:27017/sellitup';
+//require('dotenv').config();
+const connectionStr = 'mongodb://localhost:27017/ticketdiary';
 
 mongoose.connect(connectionStr, {
     useNewUrlParser: true,
