7d92dbca5ce1a5b6003c9485d275c10637c72277
brianhung7 <brian.texts@gmail.com>
2021-07-30 14:05:42 -0700

    user schema

--- server.js
+++ server.js
@@ -1,58 +1,95 @@
+//Server info
+
 /* SECTION External modules */
 const express = require("express");
 const methodOverride = require("method-override");
+// const session = require("express-session");
+// const MongoStore = require("connect-mongo");
+// require("dotenv").config();
 
 /* SECTION module instance */
 const app = express();
 
 /* SECTION PORT  */
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 /* SECTION Internal Modules */
-const controllers = require("./controllers");
+// const controllers = require("./controllers");
 
 /* SECTION App Config */
 
-app.set("view engine", "ejs");
+// app.set("view engine", "ejs");
 
+// session controller
+// app.use(
+//   session({
+//     // this will store the cookies in the mongodb database
+//     store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }),
+//     // secret key will sign the cookie for validation
+//     secret: process.env.SECRET,
+//     resave: false,
+//     saveUninitialized: false,
+//     // cookie config
+//     cookie: {
+//       maxAge: 1000 * 60 * 60 * 24 * 7 * 2, // two weeks
+//     },
+//   })
+// );
 
 /* SECTION Middleware */
 
 
-// create a location for our nav links
-app.use(require("./utils/navlinks"));
+// // NOTE this will add a variable called user to EVERY EJS file
+// app.use((req, res, next) => {
+//   res.locals.user = req.session.currentUser;
+//   return next();
+// });
 
-app.use(express.static("public"));
+// // create a location for our nav links
+// app.use(require("./utils/navlinks"));
 
-// NOTE allow body data for all routes
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.static("public"));
 
-app.use(methodOverride("_method"));
+// // NOTE allow body data for all routes
+// app.use(express.urlencoded({ extended: true }));
 
-app.use(require("./utils/logger"));
+// app.use(methodOverride("_method"));
+
+// app.use(require("./utils/logger"));
+
+// // auth required 
+// if user is authenticated 
+// do nothing 
+// if not authenticated 
+// redirect to login 
+
+// const authRequired = (req,res,next) => {
+//   if(!req.session.currentUser){
+//     return res.redirect("/login");
+//   }
+
+//   next();
+// }
+
 
 
 /* !SECTION */
-//app.get("/", (req, res) => res.redirect("/gallery"));
-app.get("/",(req, res)=>{
-    res.send("Hello");
-})
+// app.get("/", (req, res) => res.redirect("/products"));
 
 
-/* SECTION Routes 
-app.use("/", controllers.auth);
-app.use("/products", controllers.product);
-app.use("/reviews", authRequired, controllers.review);
-*/
+// /* SECTION Routes */
+// app.use("/", controllers.auth);
+// app.use("/products", controllers.product);
+// app.use("/reviews", authRequired, controllers.review);
 
-// 404
-app.get("/*", (req, res) => {
-    const context = {
-        error: req.error,
-    };
+// // 404
+// app.get("/*", (req, res) => {
+//   const context = {
+//     error: req.error,
+//   };
 
-    res.render("404", context);
-});
+//   res.render("404", context);
+// });
 
 app.listen(PORT, () =>
     console.log(`Listening for client requests on port ${PORT}`)
