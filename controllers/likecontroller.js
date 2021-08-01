45aeecff5083344e45a0b023d869649f119eaf2b
brianhung7 <brian.texts@gmail.com>
2021-08-01 14:18:21 -0700

    Like counter now increments and updates like count

--- controllers/likecontroller.js
+++ controllers/likecontroller.js
@@ -2,16 +2,36 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
 const User = require("../models/User");
+const Comment = require("../models/Comment");
 const Like = require("../models/Like");
 
-router.get("/",(req,res,next)=>{
+router.get("/", (req, res, next) => {
     res.send("hi");
 })
 
 
-router.put("/:id", async (req,res,next)=>{
-    console.log("LIke put route");
-    const foundPost = await Post.findById(req.params.id);
-    console.log(foundPost);
-})
+router.put("/:id", async (req, res, next) => {
+    try {
+        const foundPost = await Post.findById(req.params.id).populate("user");
+        const allComments = await Comment.find({ post: req.params.id }).populate("post user");
+        const foundLikes = await Like.findOne({post: req.params.id});
+        console.log("BEFORE UPDATE", foundLikes);
+        foundLikes.numLikes++;
+        await Like.findByIdAndUpdate(
+            foundLikes._id,
+            {numLikes: foundLikes.numLikes},
+            {new:true},
+        )
+        const context = {
+            post: foundPost,
+            comments: allComments,
+            likes: foundLikes,
+        }
+        return res.redirect(`/gallery/${req.params.id}`);
+    } catch (error) {
+        console.log(error);
+        req.error = error;
+        return next();
+    }
+});
 module.exports = router;
